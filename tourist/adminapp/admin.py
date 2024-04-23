5f81fb9013c116271489487a25c7831305eac8f6
MShrinath <112850938+MShrinath@users.noreply.github.com>
2024-04-23 19:02:25 +0530

    SDP admin panel

--- tourist/adminapp/admin.py
+++ tourist/adminapp/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email', 'full_name', 'gender', 'date_of_birth', 'phone_number', 'country', 'agreed_to_terms')
+    search_fields = ('username', 'email', 'full_name', 'phone_number', 'address', 'country')
+    list_filter = ('gender',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('full_name', 'date_of_birth', 'gender', 'email', 'phone_number', 'address', 'country')}),
+        ('Terms & Conditions', {'fields': ('agreed_to_terms',)}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
--- tourist/adminapp/models.py
+++ tourist/adminapp/models.py
@@ -3,21 +3,21 @@ from django.db import models
 
 class User(AbstractUser):
     full_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
+    date_of_birth = models.DateField(blank=True, null=True)
     GENDER_CHOICES = (
         ('male', 'Male'),
         ('female', 'Female'),
     )
     gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
     email = models.EmailField()
-    phone_number = models.CharField(max_length=15)  # Adjust max_length as needed
+    phone_number = models.CharField(max_length=15)  
     address = models.CharField(max_length=255)
     country = models.CharField(max_length=100)
     agreed_to_terms = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.full_name()
+        return self.get_full_name()
     
     class Meta:
-        db_table = 'user_table'  # Replace 'user' with your desired table name
+        db_table = 'user_table'  
 
